dc10761cee99ae0f7b6de047d01593f8dcc15ce7
carlososf <ccofilho.2005@gmail.com>
2026-02-19 11:36:49 -0300

    Configure GitHub Pages deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { lazy, Suspense } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import LayoutWrapper from './components/layout/LayoutWrapper';
 import { Loader2 } from 'lucide-react';
 
